657c178c447bd244c6045cdb0e34cc1dbe3e7a8c
SheHab970 <shehab202205097@gmail.com>
2025-04-26 14:55:25 +0300

    Install libraries and add nav-bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    },
+    {
+        path: 'home',
+        loadComponent: () =>
+          import('./modules/pages/home/home.component').then(
+            (c) => c.HomeComponent
+          ),
+    },
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
