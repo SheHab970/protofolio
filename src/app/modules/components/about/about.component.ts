5b59c40cee03fec63d4ea54173dfd547bd494e7b
SheHab970 <shehab202205097@gmail.com>
2025-04-28 18:40:07 +0300

    Add about section

--- src/app/modules/components/about/about.component.ts
+++ src/app/modules/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, ViewChildren, QueryList, HostListener, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,6 +7,41 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
+export class AboutComponent implements AfterViewInit{
 
+  @ViewChild('animatedElement') animatedElement!: ElementRef;
+  @ViewChild('parentContainer') parentContainer!: ElementRef;
+  @ViewChildren('child') childElements!: QueryList<ElementRef>;
+  isVisible = false;
+
+  ngAfterViewInit() {
+    this.setTransitionDelays();
+    this.checkVisibility(); // check once when loaded
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.checkVisibility();
+  }
+
+  setTransitionDelays() {
+    const children = this.parentContainer.nativeElement.querySelectorAll('.child');
+    children.forEach((child: HTMLElement, index: number) => {
+      child.style.transitionDelay = `${index * 0.3}s`; // 0s, 0.2s, 0.4s, 0.6s
+    });
+  }
+
+  checkVisibility() {
+    const rect = this.animatedElement.nativeElement.getBoundingClientRect();
+    const rect1 = this.parentContainer.nativeElement.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+  
+    if (rect.top < windowHeight - 100 && rect.bottom > 100 && rect1.top < window.innerHeight - 100) {
+      this.isVisible = true;
+      this.parentContainer.nativeElement.classList.add('show');
+    } else {
+      this.isVisible = false;
+      this.parentContainer.nativeElement.classList.remove('show');
+    }
+  }
 }
