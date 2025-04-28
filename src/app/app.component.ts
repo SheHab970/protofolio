82568c9e05cc248b16ae03b06017faa917563462
SheHab970 <shehab202205097@gmail.com>
2025-04-28 16:35:19 +0300

    Edite hero section and create about component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './core/nav-bar/nav-bar.component';
+import { AboutComponent } from './modules/components/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent],
+  imports: [RouterOutlet,NavBarComponent,AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
