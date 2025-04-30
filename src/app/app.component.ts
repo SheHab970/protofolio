6414351b428c032587abf7c32a85b4ee98c7f546
SheHab970 <shehab202205097@gmail.com>
2025-04-30 13:58:49 +0300

    create projects component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './core/nav-bar/nav-bar.component';
 import { AboutComponent } from './modules/components/about/about.component';
 import { SkillsComponent } from './modules/components/skills/skills.component';
+import { ProjectsComponent } from './modules/components/projects/projects.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent,AboutComponent,SkillsComponent],
+  imports: [RouterOutlet,NavBarComponent,AboutComponent,SkillsComponent,ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
