f161eb7bb7d22c0afe9758100f66394800a72b88
SheHab970 <shehab202205097@gmail.com>
2025-05-10 19:55:13 +0300

    Update projects and add url  for repo

--- src/app/modules/components/projects/projects.component.ts
+++ src/app/modules/components/projects/projects.component.ts
@@ -15,19 +15,22 @@ export class ProjectsComponent {
       img : 'assets/imgs/projects/CtrlPDashboard.png',
       name : 'Admin Dashbourd',
       description: 'Modern Angular dashboard with clean UI and dynamic charts.',
-      url : 'https://ctrl-p-dashboard.vercel.app/'
+      url : 'https://ctrl-p-dashboard.vercel.app/',
+      repourl : 'https://github.com/SheHab970/CtrlP-Dashboard'
     },
     {
       img : 'assets/imgs/projects/Recipes-Sharing.png',
       name : 'Recipes Sharing',
       description: 'A vibrant platform to discover, share, and save your favorite recipes.',
-      url : 'https://recipes-sharing.netlify.app/'
+      url : 'https://recipes-sharing.netlify.app/',
+      repourl : 'https://github.com/jawadTamer/recipe-sharing'
     },
     {
       img : 'assets/imgs/projects/RentalX.png',
       name : 'RentalX',
       description: 'Sleek car rental platform with smart booking and real-time availability.',
-      url : 'https://shehab970.github.io/RentaLx_website/'
+      url : 'https://shehab970.github.io/RentaLx_website/',
+      repourl : 'https://github.com/SheHab970/RentaLx_website'
     },
   ];
 
