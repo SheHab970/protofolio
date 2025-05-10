16cea93b16763c5ab5fe30c0747fed16e8aa13bc
SheHab970 <shehab202205097@gmail.com>
2025-05-10 21:58:30 +0300

    edite router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,6 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    // {
-    //     path: '',
-    //     redirectTo: 'home',
-    //     pathMatch: 'full',
-    // },
-    // {
-    //     path: 'home',
-    //     loadComponent: () =>
-    //       import('./modules/components/home/home.component').then(
-    //         (c) => c.HomeComponent
-    //       ),
-    // },
+
 ];
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
 export class AppRoutingModule {}
