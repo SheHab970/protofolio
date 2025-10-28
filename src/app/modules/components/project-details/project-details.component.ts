9dd3d7a9606857c3389f5289a28511244b55f996
SheHab970 <shehab202205097@gmail.com>
2025-10-28 23:45:05 +0300

    Add base structure of page

--- src/app/modules/components/project-details/project-details.component.ts
+++ src/app/modules/components/project-details/project-details.component.ts
@@ -1,20 +1,22 @@
-import { Component, Optional } from '@angular/core';
+import { Component, ElementRef, Optional, ViewChild } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 import { filter } from 'rxjs';
 import { ProjectsService } from '../../../core/services/projects.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-project-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project-details.component.html',
   styleUrl: './project-details.component.scss'
 })
 export class ProjectDetailsComponent {
 
   itemId : any;
+  currentItrm : any;
 
   constructor(
     private projectsService : ProjectsService,
@@ -35,9 +37,25 @@ export class ProjectDetailsComponent {
         if (id) {
           this.itemId = id;
           console.log('Received ID:', this.itemId)
+          this.loadProjectById(this.itemId)
         };
       });
     }
+
+  loadProjectById(id: number) {
+  this.currentItrm = this.projectsService.projectsData.find(p => p.id === id);
+  console.log('Loaded project:', this.currentItrm);
+}
+
+  @ViewChild('scrollDiv') scrollDiv!: ElementRef<HTMLDivElement>;
+
+  scrollLeft() {
+    this.scrollDiv.nativeElement.scrollBy({ left: -300, behavior: 'smooth' });
+  }
+
+  scrollRight() {
+    this.scrollDiv.nativeElement.scrollBy({ left: 300, behavior: 'smooth' });
+  }
 
   colsePopup(): void {
     this.activeModal.close();
