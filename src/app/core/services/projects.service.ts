cd4a26749ae4616c3724e3ee81d57e4f80a4932e
SheHab970 <shehab202205097@gmail.com>
2025-10-28 21:06:56 +0300

    Move projects information to service

--- src/app/core/services/projects.service.ts
+++ src/app/core/services/projects.service.ts
@@ -14,4 +14,49 @@ export class ProjectsService {
   setId(id: number) {
     this.idSource.next(id);
   }
+
+    projectsData = [
+    {
+      id : 1,
+      mainImg : 'assets/imgs/projects/CtrlPDashboard.png',
+      images : [
+        'assets/imgs/projects/CtrlPDashboard.png',
+        'assets/imgs/projects/CtrlPDashboard.png',
+        'assets/imgs/projects/CtrlPDashboard.png',
+        'assets/imgs/projects/CtrlPDashboard.png',
+      ],
+      name : 'Admin Dashbourd',
+      description: 'Modern Angular dashboard with clean UI and dynamic charts.',
+      url : 'https://ctrl-p-dashboard.vercel.app/',
+      repourl : 'https://github.com/SheHab970/CtrlP-Dashboard'
+    },
+    {
+      id : 2,
+      mainImg : 'assets/imgs/projects/Recipes-Sharing.png',
+      images : [
+        'assets/imgs/projects/Recipes-Sharing.png',
+        'assets/imgs/projects/Recipes-Sharing.png',
+        'assets/imgs/projects/Recipes-Sharing.png',
+        'assets/imgs/projects/Recipes-Sharing.png',
+      ],
+      name : 'Recipes Sharing',
+      description: 'A vibrant platform to discover, share, and save your favorite recipes.',
+      url : 'https://recipes-sharing.netlify.app/',
+      repourl : 'https://github.com/jawadTamer/recipe-sharing'
+    },
+    {
+      id : 3,
+      mainImg : 'assets/imgs/projects/RentalX.png',
+      images : [
+        'assets/imgs/projects/RentalX.png',
+        'assets/imgs/projects/RentalX.png',
+        'assets/imgs/projects/RentalX.png',
+        'assets/imgs/projects/RentalX.png',
+      ],
+      name : 'RentalX',
+      description: 'Sleek car rental platform with smart booking and real-time availability.',
+      url : 'https://shehab970.github.io/RentaLx_website/',
+      repourl : 'https://github.com/SheHab970/RentaLx_website'
+    },
+  ];
 }
