f9166d3b86af6df53199f8b725749e0e74978127
SheHab970 <shehab202205097@gmail.com>
2025-10-31 01:19:14 +0200

    update page

--- src/app/modules/components/project-details/project-details.component.ts
+++ src/app/modules/components/project-details/project-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Optional, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Optional, ViewChild } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
@@ -13,7 +13,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './project-details.component.html',
   styleUrl: './project-details.component.scss'
 })
-export class ProjectDetailsComponent {
+export class ProjectDetailsComponent implements AfterViewInit{
 
   itemId : any;
   currentItrm : any;
@@ -49,12 +49,37 @@ export class ProjectDetailsComponent {
 
   @ViewChild('scrollDiv') scrollDiv!: ElementRef<HTMLDivElement>;
 
+  activeIndex: number = 1;
+
+  ngAfterViewInit() {
+    this.updateActiveIndex();
+  }
   scrollLeft() {
     this.scrollDiv.nativeElement.scrollBy({ left: -300, behavior: 'smooth' });
   }
 
   scrollRight() {
     this.scrollDiv.nativeElement.scrollBy({ left: 300, behavior: 'smooth' });
+  }
+
+  scrollToIndex(index: number) {
+    const scrollContainer = this.scrollDiv.nativeElement;
+    const imageWidth = scrollContainer.clientWidth;
+    scrollContainer.scrollTo({ left: index * imageWidth, behavior: 'smooth' });
+    this.activeIndex = index;
+    
+  }
+
+  onScroll() {
+    this.updateActiveIndex();
+  }
+  private updateActiveIndex() {
+    const scrollContainer = this.scrollDiv.nativeElement;
+    const scrollLeft = scrollContainer.scrollLeft;
+    const width = scrollContainer.clientWidth;
+    const index = Math.round(scrollLeft / width);
+    // this.activeIndex = index;
+    this.activeIndex = Math.min(Math.max(index, 0), this.currentItrm.images.length - 1);
   }
 
   colsePopup(): void {
